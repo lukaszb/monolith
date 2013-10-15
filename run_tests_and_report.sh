157a88ce150252fdeb3a2c2eb26b5d5cb3be936f
Lukasz Balcerzak <lukaszbalcerzak@gmail.com>
2013-10-15 15:32:44 +0200

    Do not try to cover utils.imports module while testing

--- run_tests_and_report.sh
+++ run_tests_and_report.sh
@@ -3,6 +3,6 @@
 echo "Running test suite with coverage report at the end"
 echo -e "( would require coverage python package to be installed )\n"
 
-OMIT="monolith/compat.py,monolith/tests/__init__.py"
+OMIT="monolith/compat.py,monolith/tests/__init__.py,monolith/compat.py,monolith/utils/imports.py"
 coverage run setup.py test
 coverage report -m --include "monolith/*" --omit $OMIT
